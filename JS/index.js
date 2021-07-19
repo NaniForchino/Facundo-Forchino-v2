c12fb59fd84c326b2530710e5098d0cb1dc1663a
usuarioGithub <forchinofacundo@gmail.com>
2021-07-19 16:07:52 -0300

    First commit

---
+++ JS/index.js
@@ -0,0 +1,81 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.fixed', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.cabecera', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.frase1', {
+    duration: 5000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  
+  sr.reveal('.frase2', {
+    duration: 5000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+
+
+sr.reveal('.perfil', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.titulo-perfil', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.titulo-experiencia', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.experiencia', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.titulo-formacion', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.formacion', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.titulo-charlemos', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.charlemos', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.flecha', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
